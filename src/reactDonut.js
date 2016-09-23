6f22ec01ef7d2a83fdc9ab5210e98538abf3a9c4
Vatsalya <vatsalya_prakash@mckinsey.com>
2016-09-24 02:57:16 +0530

    initial commit with hello world

---
+++ src/reactDonut.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ReactDonut extends React.Component {
+  render() {
+    return (  
+      <div>Hello there, Vat</div>
+    )
+  }
+}
+
+export default ReactDonut;
